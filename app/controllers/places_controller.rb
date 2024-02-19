c2d81703eea0cc2f60d3d67928cf9517d10ec230
Rob Vogel <rob.vogel@kellogg.northwestern.edu>
2024-02-19 22:47:15 +0000

    sync

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,11 +4,31 @@ class PlacesController < ApplicationController
 
     @places = Place.all
     
-    end
+  end
 
-    def show
-      
+  def show
+    @place = Place.find_by("id" => params["id"])
 
-    end
+    @entries = Entry.where("place_id" => @place.id)
+  end
+
+
+  def new
+    @place = Place.new
+  end
+
+
+  def create
+    #create a new place
+    @place = Place.new
+    @place.name = params["name"]
+
+    #save place
+    @place.save
+
+    #send them back
+    redirect_to /places
+  end
 
 end
+
